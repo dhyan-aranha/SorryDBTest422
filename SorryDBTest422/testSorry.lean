a3b00452df4fc4f01aa7973a7760bc1f1fcfe6b0
dhyan-aranha <51273141+dhyan-aranha@users.noreply.github.com>
2025-10-28 11:39:00 +0100

    Update testSorry.lean

--- SorryDBTest422/testSorry.lean
+++ SorryDBTest422/testSorry.lean
@@ -1,5 +1,14 @@
 import Mathlib
-import Hammer
---import PremiseSelection
 
-lemma hi : ∀ (n :ℝ), n^2 ≥ 0 := by sorry
+
+--blah blah blah
+
+def hi : Nat := 7
+
+
+--blah blah
+
+lemma foo : ∀ (n :ℝ), n^2 ≥ 0 := by sorry
+
+
+lemma foo_a : hi^2 ≥ 0 := by sorry
